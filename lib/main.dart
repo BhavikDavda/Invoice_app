77a9a5026a9e0ef44fed6d44eb365549912400cc
BhavikDavda <158564530+BhavikDavda@users.noreply.github.com>
2024-09-04 12:19:01 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,18 @@
+import 'package:e_commerce_app/screen/invoice.dart';
 import 'package:flutter/material.dart';
-import 'package:invoice_app/screens/cart_page.dart';
-import 'package:invoice_app/screens/detail_page.dart';
-import 'package:invoice_app/screens/home_page.dart';
-import 'package:invoice_app/screens/invoice_page.dart';
+import 'package:e_commerce_app/screen/home_page.dart';
+import 'package:e_commerce_app/screen/detail_page.dart';
+import 'package:e_commerce_app/screen/cart_page.dart';
 
 void main() {
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: const Homepage(),
       routes: {
-        'Homepage': (context) => const Homepage(),
+        '/': (context) => const Homescreen(),
         'Detailpage': (context) => const Detailpage(),
-        'CartPage': (context) => const  CartPage(),
-        'bilpage': (context) => const PdfPage(),
+        'Cartpage': (context) => const Cartpage(),
+        'PdfPage': (context) => const PdfPage(),
       },
     ),
   );
